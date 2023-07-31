8b567bb9a6f48972547b18139d90e1fa1d90aa9d
partolaaa <ivan.partola77@gmail.com>
2023-07-31 10:25:19 +0200

    fixed some more style things

--- src/main/resources/static/js/header.js
+++ src/main/resources/static/js/header.js
@@ -6,6 +6,14 @@ document.getElementById('logo').onclick = function () {
     location.href='/'
 };
 
+
+document.querySelectorAll('.side-menu-element').forEach(function(element) {
+    element.addEventListener('click', function() {
+        let link = this.querySelector('a');
+        window.location.href = link.getAttribute('href');
+    });
+});
+
 document.getElementById('menu').addEventListener('click', function() {
     document.getElementById('side-menu').classList.add('open-menu');
 });
